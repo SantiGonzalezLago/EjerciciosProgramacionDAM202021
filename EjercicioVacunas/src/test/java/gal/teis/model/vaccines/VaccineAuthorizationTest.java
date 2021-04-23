e4e50a72c5a48da76891635263ff347dfbd5d761
Santi <santiago.gonzalez.lago@gmail.com>
2021-04-23 16:50:58 +0200

    Más tests unitarios

---
+++ EjercicioVacunas/src/test/java/gal/teis/model/vaccines/VaccineAuthorizationTest.java
@@ -0,0 +1,61 @@
+package gal.teis.model.vaccines;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VaccineAuthorizationTest {
+
+	VaccineAuthorization instance;
+	
+	@BeforeEach
+	void createInstance() {
+		// Creo cualquier clase que implemente esta, dado que esta es abstracta y los métodos son finales
+		instance = new JanssenVaccine("VAedf45");
+	}
+	
+	@Test
+	void testAuthorizeTrue() throws IllegalAccessException, IllegalArgumentException {
+		instance.setTestPhaseResult((byte) 1, true);
+		instance.setTestPhaseResult((byte) 2, true);
+		instance.setTestPhaseResult((byte) 3, true);
+		assertTrue(instance.authorize());
+	}
+	
+	@Test
+	void testAuthorizeFalse() throws IllegalAccessException, IllegalArgumentException {
+		instance.setTestPhaseResult((byte) 1, true);
+		assertFalse(instance.authorize());
+	}
+	
+	@Test
+	void testRejectTrue1() throws IllegalAccessException, IllegalArgumentException {
+		instance.setTestPhaseResult((byte) 1, true);
+		instance.setTestPhaseResult((byte) 2, false);
+		assertTrue(instance.reject());
+	}
+	
+	@Test
+	void testRejectTrue2() throws IllegalAccessException, IllegalArgumentException {
+		instance.setTestPhaseResult((byte) 1, true);
+		instance.setTestPhaseResult((byte) 2, true);
+		instance.setTestPhaseResult((byte) 3, false);
+		assertTrue(instance.reject());
+	}
+	
+	@Test
+	void testRejectFalse1() throws IllegalAccessException, IllegalArgumentException {
+		instance.setTestPhaseResult((byte) 1, true);
+		instance.setTestPhaseResult((byte) 2, true);
+		instance.setTestPhaseResult((byte) 3, true);
+		assertFalse(instance.reject());
+	}
+	
+	@Test
+	void testRejectFalse2() throws IllegalAccessException, IllegalArgumentException {
+		instance.setTestPhaseResult((byte) 1, true);
+		assertFalse(instance.reject());
+	}
+
+}
